94104885bfd4fa873335f42dcb962614629604aa
Mila48Drokina <Drokina.lyudmila48@yandex.ru>
2024-02-11 20:00:16 +0300

    .idea/ added

---
+++ task1.py/task1.py
@@ -0,0 +1,58 @@
+cook_book = {
+  'Омлет': [
+    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    ],
+  'Утка по-пекински': [
+    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    ],
+  'Запеченный картофель': [
+    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    ]
+  }
+'''
+
+
+def read_cookbook():
+    file_path = os.path.join(os.getcwd(), 'recipes.txt')
+    cook_book = {}
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            dish_name = line.strip()
+            count = int(f.readline())
+            ing_list = list()
+            for item in range(count):
+                ingrs = {}
+                ingr = f.readline().strip()
+                ingrs['ingredient_name'], ingrs['quantity'], ingrs['measure'] = ingr.split('|')
+                ingrs['quantity'] = int(ingrs['quantity'])
+                ing_list.append(ingrs)
+            f.readline()
+            cook_book[dish_name] = ing_list
+    return cook_book
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingr_list = dict()
+
+    for dish_name in dishes:  # итерируем список полученных блюд
+        if dish_name in cook_book:
+            for ings in cook_book[dish_name]:  # итерируем ингридиенты в блюде
+                meas_quan_list = dict()
+                if ings['ingredient_name'] not in ingr_list:
+                    meas_quan_list['measure'] = ings['measure']
+                    meas_quan_list['quantity'] = ings['quantity'] * person_count
+                    ingr_list[ings['ingredient_name']] = meas_quan_list
+                else:
+                    ingr_list[ings['ingredient_name']]['quantity'] = ingr_list[ings['ingredient_name']]['quantity'] + \
+                                                                     ings['quantity'] * person_count
+
+        else:
+            print(f'\n"Такого блюда нет в списке!"\n')
+    return ingr_list
